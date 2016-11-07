90438a8daa280b35a17f07f4dbe7b068d8f95418
Navya Ranjan <mails2navya@gmail.com>
2016-11-07 13:23:04 +1100

    LogAnalysis modified

--- HW-01/LogAnalysis.hs
+++ HW-01/LogAnalysis.hs
@@ -2,36 +2,25 @@ module LogAnalysis where
 
 import Log
 
-parseMessage :: String -> LogMessage
 --splitting words in message to list of words and parsing by patteren matching
---read x::Int used for converting String type to Int
-parseMessage message
-                    |(wordList !! 0) == "E" = LogMessage (Error ((read (wordList !! 1))::Int)) ((read (wordList !! 2))::Int) (unwords (drop 3 wordList))
-                    |(wordList !! 0) == "I" = LogMessage Info (read (wordList !! 1) :: Int) (unwords (drop 2 wordList))
-                    |(wordList !! 0) == "W" = LogMessage Warning (read (wordList !! 1) :: Int) (unwords (drop 2 wordList))
-                    |otherwise              = Unknown message
-      where wordList=words message
-
+parseMessage :: String -> LogMessage
+parseMessage message=case wordList of
+                        ("E":sev:time:msgString)->LogMessage (Error (read sev)) (read time) (unwords msgString)
+                        ("I":time:msgString)    ->LogMessage Info (read time) (unwords msgString)
+                        ("W":time:msgString)    ->LogMessage Warning (read time) (unwords msgString)
+                        _                       ->Unknown message
+      where wordList=words message  
+      
 --to parse a log file
 parse :: String -> [LogMessage]
 parse str=[parseMessage message | message <- (lines str)]
 
---getter functions on MessageTree,LogMessage and MessageType
-getMessage :: MessageTree-> LogMessage
+--getter function for timestamp
 getTimeStamp :: LogMessage -> TimeStamp
-getErrorString :: LogMessage -> String
-getSeverity :: MessageType -> Int
-getMessageType :: LogMessage -> MessageType
-
-getMessage (Node _ msg _) = msg
 getTimeStamp (LogMessage _ t _ )=t
-getErrorString (LogMessage _ _ str)=str
-getSeverity (Error value)=value
-getMessageType (LogMessage err _ _)=err
 
 --to insert LogMessages into MessageTree
 insert::LogMessage -> MessageTree -> MessageTree
-
 insert message Leaf= Node Leaf message Leaf
 insert message (Node l msg r)
                              |getTimeStamp message >= getTimeStamp msg = Node l msg (insert message r)
@@ -39,17 +28,19 @@ insert message (Node l msg r)
 
 --build a complete MessageTree from List of messages
 build ::[LogMessage] -> MessageTree
-
 build (message:[])         = insert message Leaf
 build (message:messageList)= insert message (build messageList)
 
 --inorder traversal for MessageTree
 inOrder :: MessageTree -> [LogMessage]
-
-inOrder (Leaf)        =[]
+inOrder Leaf        =[]
 inOrder (Node l msg r)=inOrder(l)++[msg]++inOrder(r)
 
 --extracting error messages with severity atleast 50
 whatWentWrong :: [LogMessage]->[String]
-whatWentWrong messageList = [getErrorString x| x<-sortedList messageList , getMessageType x/=Info && getMessageType x /= Warning && (getSeverity (getMessageType x))>=50]
-                   where sortedList x=inOrder (build (messageList))
+whatWentWrong messageList = [errorMsg | (LogMessage msgType time errorMsg)<-sortedList messageList, checkType msgType]
+                   where sortedList list   = inOrder (build (list))
+                         checkType msgType = case msgType of
+                                             Error severity -> severity >=50 
+                                             _              -> False
+                                             
